04c28ae7b2c14cca94167f9c1cc536a79311a431
Aishateo <dephaneaishs@gmail.com>
2023-10-18 14:48:29 -0700

    16. Say my name

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,6 @@
+-- List all records from the 'second_table' table with a name value, displaying score and name, ordered by descending score
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
+
