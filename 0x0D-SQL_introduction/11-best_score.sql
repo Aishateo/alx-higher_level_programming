8db7210a1f713a3f38c249266376a47db6183b6c
Aishateo <dephaneaishs@gmail.com>
2023-10-18 14:38:03 -0700

    11. Select the best

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,6 @@
+-- List all records from the 'second_table' table with score >= 10, displaying score and name, ordered by score
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
+
