0700dcb10cc37b0bbff831a41d420c5e06db5fab
Aishateo <dephaneaishs@gmail.com>
2023-10-18 14:46:36 -0700

    15. Number by score

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,7 @@
+-- List the number of records with the same score in the 'second_table' table
+-- Sorted by the number of records in descending order
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
+
