07efe6e5e432057d89540eaa9c3e9553513a64b0
Aishateo <dephaneaishs@gmail.com>
2023-10-18 15:23:55 -0700

    update 19. Temperatures #1

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- Retrieve the top 3 cities with the highest temperatures during July and August
 SELECT `city`, MAX(`value`) AS `max_temp`
 FROM `temperatures`
-WHERE `month` IN ('July', 'August')
+WHERE `month` = 7 OR `month` = 8
 GROUP BY `city`
 ORDER BY `max_temp` DESC
 LIMIT 3;
