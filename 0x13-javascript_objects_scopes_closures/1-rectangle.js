d4e4db8db130741d3ca56399706e6614a05cc9e8
Aishateo <dephaneaishs@gmail.com>
2023-11-15 11:16:28 -0800

    Update 1-rectangle.js

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
+/* eslint-disable semi */
+/* eslint-disable no-console */
 
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     this.width = w;
     this.height = h;
   }
