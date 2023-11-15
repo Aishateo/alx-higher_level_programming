66c10af2f90192a48880cd48525ca464a3b38d4d
Aishateo <dephaneaishs@gmail.com>
2023-11-15 11:31:35 -0800

    2nd update 2. Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,18 +2,22 @@
 /* eslint-disable semi */
 /* eslint-disable no-console */
 
-class Rectangle {	
-	constructor(w, h) {
-		if (w <= 0 || h <= 0) {
-			return {};
-		}
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0) {
+      // If w or h is not a positive integer, create an empty object
+      return Rectangle.createEmpty();
+    }
 
-		/* Initialize instance attributes
-		 * only if w and h are positive integers
-		 */
-		this.width = w;
-		this.height = h;
-	}
+    // Initialize instance attributes only if w and h are positive integers
+    this.width = w;
+    this.height = h;
+  }
+
+  // Static method to create an empty object
+  static createEmpty() {
+    return {};
+  }
 }
 
 module.exports = Rectangle;
