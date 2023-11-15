0069524e7f4630c458b83859fa48f434738ebcf9
Aishateo <dephaneaishs@gmail.com>
2023-11-15 11:42:31 -0800

    3rd update 2. Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,22 +1,11 @@
 #!/usr/bin/node
-/* eslint-disable semi */
-/* eslint-disable no-console */
 
 class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0) {
-      // If w or h is not a positive integer, create an empty object
-      return Rectangle.createEmpty();
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
-
-    // Initialize instance attributes only if w and h are positive integers
-    this.width = w;
-    this.height = h;
-  }
-
-  // Static method to create an empty object
-  static createEmpty() {
-    return {};
   }
 }
 
